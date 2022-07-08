94eac9530e438a51bbafed9c059d10a5fcdd8606
Christian Peña <ch.penaf@gmail.com>
2022-07-08 18:44:22 -0400

    First

---
+++ src/app/graficas/pages/graficas-doble/graficas-doble.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graficas-doble',
+  templateUrl: './graficas-doble.component.html',
+  styles: [
+  ]
+})
+export class GraficasDobleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
